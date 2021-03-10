0e5bd7913ca46b21efcf1ddfb422b7d56f67460a
Alexander Salmin <sasha-tlt@yandex.ru>
2021-03-10 16:21:32 +0400

    fix: Не показывать анимацию при запрете доступа к микрофону

--- src/createMusicRecognizer.ts
+++ src/createMusicRecognizer.ts
@@ -21,33 +21,35 @@ export const createMusicRecognizer = (voiceListener: ReturnType<typeof createVoi
         messageId: number;
         onMessage: (cb: (message: OriginalMessageType) => void) => () => void;
     }) => {
-        status = 'active';
-        voiceListener.listen(
-            (data: ArrayBuffer, last: boolean) =>
-                !last && sendVoice(new Uint8Array(data), last, MessageNames.MUSIC_RECOGNITION),
-        );
+        voiceListener
+            .listen(
+                (data: ArrayBuffer, last: boolean) =>
+                    !last && sendVoice(new Uint8Array(data), last, MessageNames.MUSIC_RECOGNITION),
+            )
+            .then(() => {
+                status = 'active';
+                off = onMessage((message: OriginalMessageType) => {
+                    if (message.status && message.status.code != null && message.status.code < 0) {
+                        stop();
+                    }
 
-        off = onMessage((message: OriginalMessageType) => {
-            if (message.status && message.status.code != null && message.status.code < 0) {
-                stop();
-            }
+                    if (
+                        message.messageId === messageId &&
+                        message.messageName.toUpperCase() === MessageNames.MUSIC_RECOGNITION
+                    ) {
+                        if (!message.bytes?.data?.length) {
+                            return;
+                        }
 
-            if (
-                message.messageId === messageId &&
-                message.messageName.toUpperCase() === MessageNames.MUSIC_RECOGNITION
-            ) {
-                if (!message.bytes?.data?.length) {
-                    return;
-                }
-
-                const { decoderResultField, errorResponse } = Music2TrackProtocol.MttResponse.decode(
-                    message.bytes.data,
-                );
-                if (decoderResultField?.isFinal || errorResponse) {
-                    stop();
-                }
-            }
-        });
+                        const { decoderResultField, errorResponse } = Music2TrackProtocol.MttResponse.decode(
+                            message.bytes.data,
+                        );
+                        if (decoderResultField?.isFinal || errorResponse) {
+                            stop();
+                        }
+                    }
+                });
+            });
     };
 
     return {
