c842b7c22cbb73d2617f5a2012570ccf4504534e
Alexander Salmin <sasha-tlt@yandex.ru>
2021-07-14 15:56:40 +0400

    fix: Неразрезолвленный доступ к микрофону

--- src/assistant/voice/createSpeechRecognizer.ts
+++ src/assistant/voice/createSpeechRecognizer.ts
@@ -28,47 +28,43 @@ export const createSpeechRecognizer = (voiceListener: ReturnType<typeof createVo
         sendVoice: (data: Uint8Array, last: boolean) => void;
         messageId: number;
         onMessage: (cb: (message: OriginalMessageType) => void) => () => void;
-    }) => {
-        voiceListener
-            .listen(sendVoice)
-            .then(() => {
-                status = 'active';
-                off = onMessage((message: OriginalMessageType) => {
-                    if (message.status && message.status.code != null && message.status.code < 0) {
-                        off();
-                        stop();
+    }) =>
+        voiceListener.listen(sendVoice).then(() => {
+            status = 'active';
+            off = onMessage((message: OriginalMessageType) => {
+                if (message.status && message.status.code != null && message.status.code < 0) {
+                    off();
+                    stop();
+                }
+
+                if (message.messageId === messageId && message.messageName === MessageNames.STT) {
+                    if (message.text) {
+                        emit('hypotesis', message.text.data || '', message.last === 1, message.messageId);
+                        if (message.last === 1) {
+                            off();
+                            stop();
+                        }
                     }
 
-                    if (message.messageId === messageId && message.messageName === MessageNames.STT) {
-                        if (message.text) {
-                            emit('hypotesis', message.text.data || '', message.last === 1, message.messageId);
-                            if (message.last === 1) {
+                    if (message.bytes?.data) {
+                        const { decoderResultField } = PacketWrapperFromServer.decode(message.bytes.data);
+
+                        if (decoderResultField && decoderResultField.hypothesis?.length) {
+                            emit(
+                                'hypotesis',
+                                decoderResultField.hypothesis[0].normalizedText || '',
+                                !!decoderResultField.isFinal,
+                                message.messageId,
+                            );
+                            if (decoderResultField.isFinal) {
                                 off();
                                 stop();
                             }
                         }
-
-                        if (message.bytes?.data) {
-                            const { decoderResultField } = PacketWrapperFromServer.decode(message.bytes.data);
-
-                            if (decoderResultField && decoderResultField.hypothesis?.length) {
-                                emit(
-                                    'hypotesis',
-                                    decoderResultField.hypothesis[0].normalizedText || '',
-                                    !!decoderResultField.isFinal,
-                                    message.messageId,
-                                );
-                                if (decoderResultField.isFinal) {
-                                    off();
-                                    stop();
-                                }
-                            }
-                        }
                     }
-                });
-            })
-            .catch(() => {});
-    };
+                }
+            });
+        });
 
     return {
         start,
