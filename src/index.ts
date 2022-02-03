15d48f5a146161cf788a265277b854d8cad4f85b
Evgeniy Semin <evgeniy.o.semin@gmail.com>
2022-02-03 21:22:14 +0300

    feat: Добавлена возможность подклеивать ArrayBuffer[] к голосовому сообщению

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,4 @@ export {
     AssistantEvents as AssistantClientEvents,
     createAssistant as createAssistantClient,
 } from './assistantSdk/assistant';
+export { createNavigatorAudioProvider } from './assistantSdk/voice/listener/navigatorAudioProvider';
